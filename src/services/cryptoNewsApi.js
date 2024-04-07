10a4a6881add2ebb8177bc8c5ded6832d4200254
Mohamed-Samahi <mohamedsamahi2021@gmail.com>
2024-04-07 05:21:10 +0200

    currencies api

---
+++ src/services/cryptoNewsApi.js
@@ -0,0 +1,26 @@
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+
+const cryptoNewsApiHeaders = {
+  "X-BingApis-SDK": "true",
+  "X-RapidAPI-Key": process.env.CRYPTO_RAPID_API_KEY,
+  "X-RapidAPI-Host": process.env.NEWS_RAPID_API_HOST,
+};
+
+const baseUrl = process.env.NEWS_BASE_URL;
+
+const createRequest = (url) => ({ url, headers: cryptoNewsApiHeaders });
+
+export const cryptoNewsApi = createApi({
+  reducerPath: "cryptoNewsApi",
+  baseQuery: fetchBaseQuery({ baseUrl }),
+  endpoints: (builder) => ({
+    getCryptoNews: builder.query({
+      query: ({ newsCategory, count }) =>
+        createRequest(
+          `/news/search?q=${newsCategory}&safeSearch=Off&textFormat=Raw&freshness=Day&count=${count}`
+        ),
+    }),
+  }),
+});
+
+export const { useGetCryptoNewsQuery } = cryptoNewsApi;
